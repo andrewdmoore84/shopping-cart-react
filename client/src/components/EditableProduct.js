c874a5ed0f33de733eae32f13ca071c87a9dec41
Andrew Moore <andrewdmoore84@gmail.com>
2022-06-13 15:29:42 -0400

    Refactor in progress: use context to add item to cart > > Still to be done: checking that the product is in stock before adding it to cart

--- client/src/components/EditableProduct.js
+++ client/src/components/EditableProduct.js
@@ -1,17 +1,47 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 
 import Product from "./Product";
 import Button from './Button';
 import DeleteButton from "./DeleteButton";
 import EditProductForm from "./EditProductForm";
+import { CartContext } from "../context/cart"
+import CartService from "../service/CartService"
 
-const EditableProduct = ({ productInfo, id, handleAddToCart}) => {
-  
+const EditableProduct = ({ productInfo, id }) => {
   const [ isEditing, setIsEditing ] = useState(false);
+  const { cart, setCart } = useContext(CartContext)
+
   const handleEditToggle = () => {
     setIsEditing(!isEditing);
   };
-  
+
+  const handleAddToCart = async (id) => {
+    const getIncreasedItemQuantityCart = (cart, updatedItem) => {
+      return cart.map(cartItem => {
+        if (cartItem.productId === updatedItem.productId) {
+          return {
+            ...cartItem,
+            quantity: cartItem.quantity + 1,
+          }
+        } else {
+          return cartItem
+        }
+      })
+    }
+
+    const { item: addedItem } = await CartService.add(id)
+
+    const isAlreadyInCart = cart.some(cartItem => cartItem.productId === addedItem.productId)
+
+    if (isAlreadyInCart) {
+      const updatedCart = getIncreasedItemQuantityCart(cart, addedItem)
+
+      setCart(updatedCart)
+    } else {
+      setCart(cart.concat(addedItem))
+    }
+  }
+
   return (
     <div className="product">
       <div className="product-details">
